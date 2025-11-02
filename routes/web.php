99e69ede5f1254d1a614220c15bebf4412f4aca3
Puren34 <tumbalproyek3213@gmail.com>
2025-11-03 00:24:21 +0700

    New

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,40 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
 
 Route::get('/login', function () {
     return view('login');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::get('/pegawai', function () {
+    return view('pegawai');
+});
+
+Route::get('/kasir', function () {
+    return view('kasir');
+});
+
+Route::get('/stok', function () {
+    return view('stok');
+});
+
+Route::get('/penjualan', function () {
+    return view('penjualan');
+});
+
+Route::get('/member', function () {
+    return view('member');
 });
